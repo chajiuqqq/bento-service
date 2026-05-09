c8ed20acfd801a3e66eba6566d0b359b51622599
caijiachen <caijiachen@pintechs.com>
2026-05-09 11:16:50 +0800

    添加保存模型的脚本

---
+++ save_model.py
@@ -0,0 +1,11 @@
+import shutil
+import bentoml
+
+local_model_dir = '/data/Qwen3.6-35B-A3B'
+
+with bentoml.models.create(
+    name='Qwen3.6-35B-A3B', # Name of the model in the Model Store
+) as model_ref:
+    # Copy the entire model directory to the BentoML Model Store
+    shutil.copytree(local_model_dir, model_ref.path, dirs_exist_ok=True)
+    print(f"Model saved: {model_ref}")
